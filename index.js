11bb11a3d569043e57a3a1e318e3a28479c1aa10
ShreyashB-11 <shreyash.bhovate@qsstechnosoft.com>
2025-07-30 14:37:34 +0530

    remove env

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/deploy-my-vite-app', async (req, res) => {
   }
 });
 
+
 app.listen(process.env.PORT, () => {
   console.log(`Deploy API running on http://localhost:${process.env.PORT}`);
 });
